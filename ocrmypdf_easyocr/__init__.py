78a021b1b680a59f6c16ed853f29ca4a028f5740
박현우 <phu54321@naver.com>
2024-01-21 20:58:25 +0900

    feat: _ocrThread

--- ocrmypdf_easyocr/__init__.py
+++ ocrmypdf_easyocr/__init__.py
@@ -7,21 +7,25 @@ from __future__ import annotations
 
 import logging
 import os
-from multiprocessing import Semaphore
+import multiprocessing
+import multiprocessing.managers
+import threading
 
 import cv2 as cv
 import easyocr
 import pluggy
 from ocrmypdf import OcrEngine, hookimpl
 from ocrmypdf._exec import tesseract
+import time
 
 from ocrmypdf_easyocr._cv import detect_skew
 from ocrmypdf_easyocr._easyocr import tidy_easyocr_result
 from ocrmypdf_easyocr._pdf import easyocr_to_pikepdf
 
-log = logging.getLogger(__name__)
+from typing import Optional, Tuple
+import numpy.typing as npt
 
-GPU_SEMAPHORE = Semaphore(3)
+log = logging.getLogger(__name__)
 
 ISO_639_3_2: dict[str, str] = {
     "afr": "af",
@@ -89,11 +93,46 @@ ISO_639_3_2: dict[str, str] = {
     "vie": "vi",
 }
 
+Task = Tuple[npt.NDArray, multiprocessing.Value, threading.Event]
+
+def _ocrThread(q: multiprocessing.Queue[Task], options):
+    reader: Optional[easyocr.Reader] = None
+
+    # TODO: signal _ocrThread to quit after OCR completes.
+    while True:
+        (gray, outputDict, event) = q.get()
+
+
+        # Init reader on first OCR attempt: Wait until `options` variable is fully initialized.
+        # Note: `options` variable is on the same process with the main thread.
+        try:
+            if reader is None:
+                useGPU = options.gpu
+                languages = [ISO_639_3_2[lang] for lang in options.languages]
+                reader = easyocr.Reader(languages, useGPU)
+            outputDict["output"] = reader.readtext(gray)
+        except Exception as e:
+            print(e)
+            outputDict["output"] = ""
+        finally:
+            event.set()
+
 
 @hookimpl
 def initialize(plugin_manager: pluggy.PluginManager):
     pass
 
+
+@hookimpl
+def check_options(options):
+    m = multiprocessing.Manager()
+    q = multiprocessing.Queue(-1)
+    t = threading.Thread(target=_ocrThread, args=(q, options), daemon=True)
+    t.start()
+    options._easyocr_struct = {
+        "manager": m,
+        "queue": q
+    }
 
 @hookimpl
 def add_options(parser):
@@ -143,15 +182,19 @@ class EasyOCREngine(OcrEngine):
 
     @staticmethod
     def generate_pdf(input_file, output_pdf, output_text, options):
-        languages = [ISO_639_3_2[lang] for lang in options.languages]
-
         img = cv.imread(os.fspath(input_file))
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        with GPU_SEMAPHORE:
-            reader = easyocr.Reader(languages, gpu=options.gpu)
-            raw_results = reader.readtext(gray)
-        results = [tidy_easyocr_result(r) for r in raw_results]
 
+        s = options._easyocr_struct
+        manager: multiprocessing.managers.SyncManager = s["manager"]
+        queue: multiprocessing.Queue[Task] = s["queue"]
+        outputDict = manager.dict()
+        event = manager.Event()
+        queue.put((gray, outputDict, event))
+        event.wait()
+        raw_results = outputDict["output"]
+
+        results = [tidy_easyocr_result(r) for r in raw_results]
         text = " ".join([result.text for result in results])
         output_text.write_text(text)
 
